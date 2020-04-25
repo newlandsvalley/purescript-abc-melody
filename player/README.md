59ed64633b72aa11b643a7c6a04b54d27abf74ad
John Watson <john.watson@gmx.co.uk>
2020-04-25 16:47:08 +0100

    Add a player
    This now plays the sample tune at the correct tempo.

---
+++ player/README.md
@@ -0,0 +1,7 @@
+Soundfont ABC player component example
+======================================
+
+This is a Halogen component that displays a player widget that will play any melody that conforms to the __Playable__ typeclass (representing melodies composed of a succession of phrases of __MidiNote__). The typeclass is defined in [purescript-soundfonts](https://github.com/newlandsvalley/purescript-soundfonts) and this player uses the PlayableABC instance.
+
+The player materialises the melody only when the play button is first pressed. The idea is to
+allow the player to be rendered even though the user may not wish immediately to play the melody.  Playable melodies group the notes into a succession of phrases which are the units of interruptibility. This design represents a compromise between allowing the playback to be paced properly and allowing it to be interrupted without waiting too long for this to happen.
---
+++ player/src/Main.purs
@@ -0,0 +1,56 @@
+module Player.Main where
+
+import Audio.SoundFont (Instrument, loadRemoteSoundFonts)
+import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
+import Effect.Console (log)
+import Data.Either (Either(..), fromRight)
+import Data.Abc.Parser (parse)
+import Data.Abc.PlayableAbc (TempoedAbc(..))
+import Data.Midi.Instrument (InstrumentName(..))
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+import Halogen.PlayerComponent (component)
+import Prelude (Unit, unit, bind, discard, pure, (<>), ($))
+loadInstruments :: Aff (Array Instrument)
+loadInstruments =
+  loadRemoteSoundFonts [ AcousticGrandPiano ]
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  instruments <- H.liftAff loadInstruments
+  let
+    abcText = augustsson
+    etune = parse abcText
+  body <- HA.awaitBody
+  case etune of
+    Right abcTune -> do
+      let
+        tempoedAbc = TempoedAbc { bpm : 120, abcTune: abcTune }
+      io <- runUI (component tempoedAbc instruments) unit body
+      pure unit
+    Left err -> do
+      _  <- liftEffect $ log "ABC failed to load"
+      pure unit
+  {- if we want to change the Playable recording, we can use this:
+  _ <- io.query $ H.action $ HandleNewPlayable (MidiRecording recording)
+  -}
+  pure unit
+
+augustsson :: String
+augustsson =
+  "X:1\r\n"
+  <> "T:Engelska efter Albert Augustsson\r\n"
+  <> "N:From the playing of Albert Augustsson, Bohuslän county.\r\n"
+  <> "M:4/4\r\n"
+  <> "R:Engelska\r\n"
+  <> "S:Orust\r\n"
+  <> "Z:John Watson 24/01/2015\r\n"
+  <> "L:1/8\r\n"
+  <> "K:A\r\n"
+  <> "A>c|: e2f2 efed | c2a2 e3d | cedc BdcB | Aced cBAc |\r\n"
+  <> "e2f2 efed | c2a2 e3d | cedc BdcB | A4 A>AA>B :|\r\n"
+  <> "|: e2e2 e2de | f2ed B3c | d3c d2cd | e3d cdBc |\r\n"
+  <> "A2a2 a2gf | e2f2 e3d | cedc BdcB |1 A4 A>AA>B :|2 [A4E4] [A4E4] |\r\n"
